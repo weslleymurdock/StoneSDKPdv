e4a3fedf2f63b7af32bde94bc255e39c806a4154
Weslley Murdock <weslleyluiz@outlook.com.br>
2024-06-11 19:58:18 -0300

    alternative to pay in  main activity

--- src/PoraoVendasApp/Platforms/Android/MainActivity.cs
+++ src/PoraoVendasApp/Platforms/Android/MainActivity.cs
@@ -20,68 +20,9 @@ namespace PoraoVendasApp
         {
             base.OnCreate(savedInstanceState);
 
-            StrongReferenceMessenger.Default.Register<PayMessage>(this, (r, m) =>
-            {
-                Android.Net.Uri.Builder uriBuilder = new Android.Net.Uri.Builder();
-                uriBuilder.Authority("pay");
-                uriBuilder.Scheme("payment-app");
-
-                uriBuilder.AppendQueryParameter("amount", "200");
-                uriBuilder.AppendQueryParameter("editable_amount", "0"); // 1 = true and 0 = false
-                uriBuilder.AppendQueryParameter("transaction_type", "credit"); //DEBIT, CREDIT, VOUCHER, INSTANT_PAYMENT, PIX
-                uriBuilder.AppendQueryParameter("installment_type", "merchant"); //MERCHANT, ISSUER, NONE
-                uriBuilder.AppendQueryParameter("return_scheme", "poraovendasapp");
-
-                String order_id = "";
-                if (order_id != null)
-                {
-                    uriBuilder.AppendQueryParameter("order_id", order_id);
-                }
-
-                String installment_count = "2";
-                if (installment_count != null)
-                {
-                    uriBuilder.AppendQueryParameter("installment_count", installment_count); // 2 to 99
-                }
-
-                Intent i = new Intent(Intent.ActionView);
-                i.AddFlags(ActivityFlags.NewTask);
-                i.SetData(uriBuilder.Build());
-                StartActivity(i);
-            });
-
+            
         }
-        protected override void OnActivityResult(int requestCode, Result resultCode, Intent? data)
-        {
-            base.OnActivityResult(requestCode, resultCode, data);
-            try
-            {
-                if (data != null)
-                {
-                    StrongReferenceMessenger.Default.Send(new ReturnMessage(data!.DataString!));
-                }
-            }
-            catch (Exception e)
-            {
-                StrongReferenceMessenger.Default.Send(new ReturnMessage(e.Message));
-            }
-        }
-
-        protected override void OnNewIntent(Intent intent)
-        {
-            base.OnNewIntent(intent);
-            try
-            {
-                if (intent != null)
-                {
-                    StrongReferenceMessenger.Default.Send(new ReturnMessage(intent!.DataString!));
-                }
-            }
-            catch (Exception e)
-            {
-                StrongReferenceMessenger.Default.Send(new ReturnMessage(e.Message));
-            }
-        }
+        
 
     }
 }
